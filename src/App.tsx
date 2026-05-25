5127a2c60f9bc3ca354b7f5540ac8eba8ba70182
Kanatbek <kanatbekabad@gmail.com>
2026-05-25 10:46:04 +0500

    feat: fetch weather data

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,31 @@ import WeatherCard from "./components/WeatherCard";
 
 function App() {
     const [city, setCity] = useState<string>("");
+    const [temp, setTemp] = useState<number | null>(null);
+
+    async function fetchWeather() {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${import.meta.env.VITE_API_KEY}&units=metric`);
+        
+        const data = await response.json();
+
+        setTemp(data.main.temp);
+    }
 
     return (
         <>
           <Header />
+
           <SearchInput
            city={city}
            setCity={setCity} />
+
+           <button
+           onClick={fetchWeather}
+           >Search</button>
+
           <WeatherCard
            city={city}
-           temp={23}
+           temp={temp ?? 0}
           />
         </>
     );
